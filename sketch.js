f6cbeb676c9582af9b82d407b8e8bd7fd315b217
Teddy <edmondteddy@hotmail.fr>
2023-05-08 17:52:53 +0200

    radial experiment guided by gorilla sun tutorial

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+const radius = 300
+const rings = 40
+const numPoints = 500
+
 function setup() {
-  createCanvas(800, 500)
+  createCanvas(800, 800)
 }
 
 function draw() {
-  background(0)
+  noFill()
+  background(220)
+  for (let r = 20; r < radius; r += radius / rings) {
+    beginShape()
+    for (a = 0; a < TAU; a += TAU / numPoints) {
+      strokeWeight(2)
+      var x = width / 2 + r * cos(a)
+      var y = height / 2 + r * sin(a)
+
+      curveVertex(x + (random(5) / 0.005) * TAU, y + (random(5) / 0.005) * TAU)
+    }
+    endShape(CLOSE)
+  }
+
+  noLoop()
 }
