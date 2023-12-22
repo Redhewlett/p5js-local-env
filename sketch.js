47a998a7ea74f90498c5cdef5550e4b4bdfc4059
Teddy <edmondteddy@hotmail.fr>
2023-12-22 16:44:47 +0100

    insect

--- sketch.js
+++ sketch.js
@@ -1,7 +1,40 @@
+class Walker {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  show() {
+    stroke(0)
+    strokeWeight(1)
+    point(this.x, this.y)
+  }
+  step() {
+    let choice = parseInt(random(4))
+
+    switch (choice) {
+      case 0:
+        this.x++
+        break
+      case 1:
+        this.x--
+        break
+      case 2:
+        this.y++
+        break
+      default:
+        this.y--
+    }
+  }
+}
+let walker
 function setup() {
-  createCanvas(800, 500)
+  createCanvas(800, 800)
+  background(255)
+  walker = new Walker(width / 2, height / 2)
 }
 
 function draw() {
-  background(0)
+  walker.show()
+  walker.step()
 }
